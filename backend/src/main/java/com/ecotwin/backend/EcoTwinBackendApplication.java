51320f9c415fbd07e80285af6b1ff4c8e67fe8c2
sahith0405 <sahithpudari15@gmail.com>
2026-09-10 20:44:05 +0530

    Use credentials from Render database URL

--- backend/src/main/java/com/ecotwin/backend/EcoTwinBackendApplication.java
+++ backend/src/main/java/com/ecotwin/backend/EcoTwinBackendApplication.java
@@ -3,6 +3,8 @@ package com.ecotwin.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.URI;
+
 @SpringBootApplication
 public class EcoTwinBackendApplication {
 
@@ -15,6 +17,14 @@ public class EcoTwinBackendApplication {
 
 			if (datasourceUrl.startsWith("postgresql://")) {
 				System.setProperty("spring.datasource.url", "jdbc:" + datasourceUrl);
+
+				URI parsedUrl = URI.create(datasourceUrl);
+				String userInfo = parsedUrl.getUserInfo();
+				if (userInfo != null && userInfo.contains(":")) {
+					String[] credentials = userInfo.split(":", 2);
+					System.setProperty("spring.datasource.username", credentials[0]);
+					System.setProperty("spring.datasource.password", credentials[1]);
+				}
 			}
 		}
 
